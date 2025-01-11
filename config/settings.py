3670e2406ba2f14f6dc767803fe459e517121d39
Wendy Smoak <wsmoak@gmail.com>
2025-01-11 12:25:40 -0500

    Switch to Postgres and add psycopg2
    https://docs.djangoproject.com/en/5.1/intro/tutorial02/#database-setup

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_polls_playwright_demo",
+        "USER": "demo_app_user",
+        "PASSWORD": "supsersecret",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
