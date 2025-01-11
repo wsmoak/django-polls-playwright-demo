6d8107b18b4b4b79ed7fef84651a0c21786be230
Wendy Smoak <wsmoak@gmail.com>
2025-01-11 16:07:35 -0500

    Update polls app from tutorial through part 5, excluding tests
    The tests from the tutorial are excluded because we want to use pytest.
    
    https://docs.djangoproject.com/en/5.1/intro/tutorial05/

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,72 @@
+# https://docs.djangoproject.com/en/5.0/intro/tutorial03/#a-shortcut-get-object-or-404
+from django.shortcuts import get_object_or_404
+
+# https://docs.djangoproject.com/en/5.0/intro/tutorial03/#raising-a-404-error
+from django.http import Http404
+
+# https://docs.djangoproject.com/en/5.0/intro/tutorial03/
+from django.template import loader
+
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# https://docs.djangoproject.com/en/5.0/intro/tutorial01/#write-your-first-view
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+
+# https://docs.djangoproject.com/en/5.0/intro/tutorial04/#write-a-minimal-form
+from django.urls import reverse
+
+# https://docs.djangoproject.com/en/5.0/intro/tutorial04/#use-generic-views-less-code-is-better
+from django.views import generic
+
+from .models import Choice, Question
+
+# https://docs.djangoproject.com/en/5.0/intro/tutorial04/#write-a-minimal-form
+from django.db.models import F
+
+# https://docs.djangoproject.com/en/5.0/intro/tutorial05/#improving-our-view
+from django.utils import timezone
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
