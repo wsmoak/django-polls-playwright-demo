0f2f9ca49388a6016087dfafca6e347c8c7185e5
Wendy Smoak <wsmoak@gmail.com>
2025-01-29 18:27:08 -0500

    Fix pytest.mark.django_db and update comments

--- tests/polls/test_ui_sr.py
+++ tests/polls/test_ui_sr.py
@@ -9,17 +9,16 @@ def polls_page(page, live_server):
 
 
 # Test that the question added in the 0002_add_initial_data.py migration is visible on the index page
-@pytest.mark.db(serialized_rollback=True)
+@pytest.mark.django_db(serialized_rollback=True)
 def test_initial_polls_index_page_with_data_from_migration(polls_page):
     polls_page.navigate()
 
     expect(polls_page.page.get_by_text('What is your favorite holiday?')).to_be_visible()
 
 
-# Show that serialized_rollback is NOT working as expected to restore the database after each test
-@pytest.mark.db(serialized_rollback=True)
+# Show that serialized_rollback is working as expected to restore the database after each test
+@pytest.mark.django_db(serialized_rollback=True)
 def test_that_serialized_rollback_restores_migrated_data(polls_page):
     polls_page.navigate()
 
-    # This fails, the database has been flushed and the data from the migration is gone
     expect(polls_page.page.get_by_text('What is your favorite holiday?')).to_be_visible()
